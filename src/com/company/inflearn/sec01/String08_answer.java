8416d48986bcf0dc4ba33b5424619eb29a0e1912
gunny6026 <gunny6026@naver.com>
2021-04-28 17:31:20 +0900

    8. 유효한 팰린드롬 ( replaceAll 정규식 , StringBuilder)

---
+++ src/com/company/inflearn/sec01/String08_answer.java
@@ -0,0 +1,26 @@
+package com.company.inflearn.sec01;
+
+import java.util.Scanner;
+
+public class String08_answer {
+    public String solution(String str) {
+        String answer ="NO";
+
+        str = str.toUpperCase().replaceAll("[^A-Z]","");
+                                                // 대문자 알파벳이 아니면 전부 빈 문자 ""로 해라
+
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) answer ="YES";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String08_answer T = new String08_answer();
+        System.out.println(T.solution(str));
+    }
+}
