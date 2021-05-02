602312984c94c6cf7213d0a290c0a4d14d55b41d
gunny6026 <gunny6026@naver.com>
2021-05-02 15:07:43 +0900

    11. 임시반장 ( 3중 for문 ... 복습 필수  )

--- src/com/company/inflearn/sec02/Array11.java
+++ src/com/company/inflearn/sec02/Array11.java
@@ -1,24 +1,39 @@
 package com.company.inflearn.sec02;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Array11 {
 
-    public int solution(int num , int array[][]){
-        int answer = 0;
-        //학생
-        for (int i=0; i<num; i++){
-            //학년
-            for (int j=0; j<5; j++){
-                answer= array[j][i];
+    public int solution(int num, int array[][]) {
+
+        ArrayList<Integer> ex = new ArrayList<>();
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int cnt = 0;
+        int min = Integer.MIN_VALUE;
+        for (int i = 0; i < num; i++) {
+            for (int z = 0; z < 5; z++) {
+
+                int student = array[i][z];
+                for (int j = 0; j < num; j++) {
+                    if (z != j && student == array[j][z] && !ex.contains(j)) {
+                        cnt++;
+                        ex.add(j);
+                    }
+                }
+
             }
+             if (min < cnt) {
+                hash.put(i, cnt);
+                min = cnt;
+            }
+
         }
 
 
-
-        return  answer;
+        return min + 1;
     }
-
 
 
     public static void main(String[] args) {
@@ -30,10 +45,11 @@ public class Array11 {
 
         int array[][] = new int[num][5];
 
-        for (int i=0; i<num; i++){
-            for (int j=0; j<5; j++){
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < 5; j++) {
                 array[i][j] = sc.nextInt();
             }
         }
+        System.out.println(A.solution(num, array));
     }
 }
