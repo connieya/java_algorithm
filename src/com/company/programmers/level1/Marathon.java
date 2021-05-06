28ece1f3f417c1a0e645b6280717ef39e2ce9da2
gunny6026 <gunny6026@naver.com>
2021-05-06 19:36:37 +0900

    완주하지 못한 선수 통과

--- src/com/company/programmers/level1/Marathon.java
+++ src/com/company/programmers/level1/Marathon.java
@@ -1,46 +1,34 @@
 package com.company.programmers.level1;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Marathon {
 
     public String solution(String participant[], String completion[]) {
 
-        String answer = "";
+        String answer ="";
 
-        for (int i = 0; i < participant.length; i++) {
-            boolean flag = true;
-            for (int j = 0; j < completion.length; j++) {
-                if (participant[i].equals(completion[j])) {
-                    flag = false;
-                    completion[j] = "";
-                }
-            }
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
-            if (flag) {
+        int i;
+        for ( i =0; i< completion.length; i++){
+            if (!participant[i].equals(completion[i])){
                 answer = participant[i];
+                break;
             }
-
-
         }
-
-
-        return answer;
+      return participant[i];
     }
 
     public static void main(String[] args) {
 
-//        Scanner sc = new Scanner(System.in);
-//        int num1 = sc.nextInt();
-//
-//        String participant[] = new String[num1];
-//        String completion[] = new String[num1-1];
         String participant[] = new String[]{"mislav", "stanko", "mislav", "ana"};
         String completion[] = new String[]{"stanko", "ana", "mislav"};
 
-        System.out.println(participant.length);
-        System.out.println(completion.length);
-        System.out.println(participant[3].equals(completion[1]));
+
 
         Marathon M = new Marathon();
         System.out.println(M.solution(participant, completion));
