a58c6af31271611649584e6dcb905444a7937dff
gunny6026 <gunny6026@naver.com>
2021-05-06 18:06:50 +0900

    4. 후위식 연산(postfix) Stack 사용

---
+++ src/com/company/inflearn/sec05/Main5.java
@@ -0,0 +1,17 @@
+package com.company.inflearn.sec05;
+
+
+import java.util.Scanner;
+
+public class Main5 {
+
+
+    public void solution() {
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
