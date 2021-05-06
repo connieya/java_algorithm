b08bc378db33e849802cfcbe22085f7c90b18d55
gunny6026 <gunny6026@naver.com>
2021-05-06 17:33:19 +0900

    프로그래머스 level1 - 크레인 인형뽑기

--- src/com/company/programmers/level1/Crane.java
+++ src/com/company/programmers/level1/Crane.java
@@ -1,23 +1,31 @@
 package com.company.programmers.level1;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Crane {
-    public int solution(int[][] board ,int[] moves){
+    public int solution(int[][] board, int[] moves) {
         int answer = 0;
 
-        for (int i=0; i< moves.length; i++){
-            int m = moves[i];
-            for (int k=1; k<board.length; k++){
-                for (int j=1; j<board.length; j++){
-                    if (m ==k && board[j][k] ==0){
-                        break;
-                    }else {
-                    }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Integer.MAX_VALUE);
+        for (int x : moves){
+            for (int i = 0; i< board.length; i++){
+                if (board[i][x-1] != 0 && stack.peek() != board[i][x-1]){
+                    stack.push(board[i][x-1]);
+                    board[i][x-1] = 0;
+                    break;
+                }else if (stack.peek() == board[i][x-1]){
+                    stack.pop();
+                    board[i][x-1] = 0;
+                    answer +=2;
+                    break;
                 }
             }
         }
+
         return answer;
+
     }
 
     public static void main(String[] args) {
@@ -25,15 +33,19 @@ public class Crane {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int board[][] = new int[n+1][n+1];
+        int board[][] = new int[n][n];
 
-        for (int i=1; i<=n; i++){
-            for (int j=1; j<=n; j++){
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
                 board[i][j] =sc.nextInt();
             }
         }
-        int moves[] = new int[]{1,5,3,5,1,2,1,4};
+        int m = sc.nextInt();
 
+        int moves[] = new int[m];
+        for (int k=0; k<m; k++){
+            moves[k] =sc.nextInt();
+        }
         Crane c = new Crane();
         System.out.println(c.solution(board,moves));
 
