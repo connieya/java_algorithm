9104fa0099a1174c62aaa84a452573a217ff9815
gunny6026 <gunny6026@naver.com>
2021-05-06 19:55:25 +0900

    5. 쇠막대기 (stack)

--- src/com/company/inflearn/sec05/Main5.java
+++ src/com/company/inflearn/sec05/Main5.java
@@ -2,16 +2,36 @@ package com.company.inflearn.sec05;
 
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main5 {
 
 
-    public void solution() {
+    public int solution(String str) {
+
+        int answer =0;
+        Stack<Character> stack = new Stack<>();
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop();
+                if (str.charAt(i-1)== '(') answer += stack.size();
+                else  answer++;
+            }
+
+        }
+
+        return answer;
+
 
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        Main5 T = new Main5();
+        System.out.println(T.solution(str));
     }
 }
