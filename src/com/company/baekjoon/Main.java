f369d5413158cccb82d14da4c6c9ef4a739c3208
gunny6026 <gunny6026@naver.com>
2021-06-01 21:53:27 +0900

    BOC 06/01

--- src/com/company/baekjoon/Main.java
+++ src/com/company/baekjoon/Main.java
@@ -2,30 +2,49 @@ package com.company.baekjoon;
 
 import java.util.Scanner;
 public class Main {
+    public static boolean next_permutation(int []a){
+        int i = a.length-1;
+        while (i>0 && a[i-1]>a[i]){
+             i -=1;
+        }
+
+        if (i<=0){
+            return false;
+        }
+
+        int j = a.length-1;
+        while (a[i-1] > a[j]){
+            j -=1;
+        }
+
+        int temp = a[i-1];
+        a[i-1] = a[j];
+        a[j] = temp;
+
+        j = a.length-1;
+        while (i<j){
+            temp = a[j];
+            a[j] = a[i];
+            a[i] = temp;
+            i+=1;
+            j-=1;
+        }
+        return true;
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
-        int w = sc.nextInt();
         int n = sc.nextInt();
-
-        int x =1, y =1 ,count =1;
-        for (int i=0; i<h*w; i++){
-            if (x>h){
-                x =1;
-                y+=1;
-            }
-            if (n==count){
-                break;
-            }
-            x =x+1; count+=1;
+        int a[] = new int[n];
+        for (int i=0; i<n; i++){
+            a[i] = sc.nextInt();
         }
-        StringBuilder sb = new StringBuilder();
-        if (y<10){
-            sb.append(x).append(0).append(y);
-            System.out.println(sb);
-        }else{
-            sb.append(x).append(y);
-            System.out.println(sb);
+        if (next_permutation(a)){
+            for (int x: a){
+                System.out.print(x+" ");
+            }
+        }else {
+            System.out.println(-1);
         }
 
     }
